be80decf2b5c0307fad06ec9ac16cfa6521ecb88
pantor <lars.berscheid@online.de>
2020-04-17 15:10:44 +0200

    add more samples

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,24 @@
+Vue.component('sample-state-space', {
+    props: ['id'],
+    template: '#sample-state-space',
+    data() {
+      return {
+        current_step: 0,
+      }
+    },
+    methods: {
+      
+    }
+});
+
 var v = new Vue({
     el: '#content',
     data: {
-      
+      showAllSamples: false,
+    },
+    methods: {
+        showMore: function() {
+            this.showAllSamples = true;
+        },
     }
 });
